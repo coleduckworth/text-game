f1e6133d42ee7a775b498faf5933a2e49c4117d9
Cole Duckworth <duckworth3601@gmail.com>
2023-05-03 09:50:55 -0600

    Created most of the game systems

--- Spaces.py
+++ Spaces.py
@@ -1,17 +1,17 @@
 MOVEMENT = {"east" : 0,
-			"north" : 1
-			"up" : 2
-			"west" : 3
-			"south" : 4
+			"north" : 1,
+			"up" : 2,
+			"west" : 3,
+			"south" : 4,
 			"down" : 5}
 
 class Space():
 	def __init__(self):
-		self.result_item = ""
-		self.usable_item = ""
 		self.allowed_movements = []
-		self.description = ""
-		self.__state = 0
+		self.descriptions = ""
+	
+	def description(self):
+		return self.descriptions
 	
 	def move(self, player):
 		print(f"You can go in the following directions:\n{self.allowed_movements}")
@@ -28,23 +28,90 @@ class Space():
 				print(f"You can't move {direction} from here.")
 		else:
 			print(f"'{direction}' isn't a valid direction")
+
+class Interactable(Space):
+	def __init__(self):
+		super().__init__()
+		self.usable_item = ""
+		self.descriptions = {}
+		self.affected_room = (0, 0, 0)
+		self.state = "UNACTIVATED"
+		self.interactable = True
+	
+	def description(self):
+		return self.descriptions[self.state]
 	
 	def interact(self, player):
-		if not self.__state:
+		if self.state == "UNACTIVATED" and self.interactable:
 			if self.usable_item:
 				print(f"Your inventory:\n{player.inventory}")
-				item = input("What item would you like to 'use'?\n").lower()
+				item = input("You can use an item here.\nWhat item would you like to 'use'?\n").lower()
 				if item in player.inventory:
 					if item == self.usable_item:
 						print(item)
 						player.inventory.remove(item)
-						self.usable_item = ""
-						self.__state = 1
+						self.state = "ACTIVATED"
+						spaces[self.affected_room].state = "ACTIVE"
 					else:
 						print(f"You can't use {item} here.")
 				else:
 					print(f"You don't have {item} in your inventory.")
 			else:
+				self.state = "ACTIVATED"
+				spaces[self.affected_room].state = "ACTIVE"
+		elif self.state == "ACTIVATED":
+			print("There's nothing to do here.")
+		if self.state == "ACTIVE":
+			self.action(player)
+			self.state = "ACTIVATED"
+	
+	def action(self, player):
+		pass
 
-		else:
-			print("There's nothing to interact with here.")
+class Door(Interactable):
+	def __init__(self):
+		super().__init__()
+		self.blocked_movement = ""
+
+	def action(self, player):
+		self.allowed_movements.append(self.blocked_movement)
+
+class Dispenser(Interactable):
+	def __init__(self):
+		super().__init__()
+		self.result_item = ""
+
+	def action(self, player):
+		print(f"You pickup the {self.result_item}.")
+		player.inventory.append(self.result_item)
+
+spaces = {}
+
+s = Interactable()
+s.affected_room = (0,2,0)
+s.allowed_movements.append("north")
+s.descriptions["UNACTIVATED"] = "unused"
+s.descriptions["ACTIVATED"] = "used"
+spaces[(0,0,0)] = s
+
+s = Door()
+s.affected_room = (0,1,0)
+s.allowed_movements.append("south")
+s.blocked_movement = "north"
+s.descriptions["UNACTIVATED"] = "door"
+s.descriptions["ACTIVATED"] = "opened door"
+spaces[(0,1,0)] = s
+
+s = Dispenser()
+s.allowed_movements.append("south")
+s.interactable = False
+s.descriptions["UNACTIVATED"] = "other unused"
+s.descriptions["ACTIVE"] = "There's a stick."
+s.descriptions["ACTIVATED"] = "other used"
+s.result_item = "stick"
+spaces[(0,2,0)] = s
+"""
+
+s = Space()
+s.allowed_movements.append("north")
+"""
--- main.py
+++ main.py
@@ -1,18 +1,33 @@
-import Spaces
+from Spaces import spaces
 import pickle
+from os import remove, system
+from os.path import exists
 
-ACTIONS=("quit",
-		 "save",
-		 "load",
-		 "interact",
-		 "move")
+ACTIONS = ("menu", "interact", "inventory", "move")
+
+menu_options = ["new", "quit"]
+if exists("game.dat"):
+	menu_options.append("save", "load")
 
 class Player():
 	def __init__(self):
-		self.position = [0, 0, 0]
+		self.__position = [0, 0, 0]
 		self.inventory = []
+	
+	@property
+	def position(self):
+		return self.__position
+	
+	@position.setter
+	def position(self, new_position):
+		self.__position = new_position
 
-def command():
+
+def valid_input(answer_set, prompt = "Input action:"):
+	while (command := input(f"Possible inputs: {answer_set}\n{prompt} ").lower()) not in answer_set:
+		print("Invalid input.")
+	print()
+	return command
 	
 def save():
 	with open('game.dat','wb') as f:
@@ -21,25 +36,69 @@ def save():
 	print("Game saved.")
 
 def load():
+	"""Loads the game data from game.dat"""
 	global player
 	global spaces
+
 	try:
 		with open("game.dat",'rb') as f:
 			player = pickle.load(f)
 			spaces = pickle.load(f)
 		print("Game loaded.")
 	except FileNotFoundError:
-		print("File not found.")
+		print("Game file not found")
 
-def main(player):
+def game_loop():
 	choice = None
-	space = spaces.get(player.position, "There's not a space at that location. Code's broken.")
-	print(space.description)
-	choice = command()
-	while choice != "quit":
+	while choice != "menu":
+		space = spaces.get(tuple(player.position), "There's not a space at that location. Code's broken.")
+		print(space.description() + "\n")
+		choice = valid_input(ACTIONS)
 		match choice:
 			case "move":
 				space.move(player)
-			case: "quit"
+			case "interact":
+				space.interact(player)
+			case "inventory":
+				print(f"Your inventory:\n{player.inventory}")
+			case "menu":
+				menu_options.append("resume")
+
+def clear():
+	system("cls")
+	#system("clear")
+
+player = Player()
+def main(player):
+	clear()
+	choice = None
+	while choice != "quit":
+		print(""" _____    _____     ____     ____
+|  __ \  |  __ \   / __ \   / ___\ 
+| |__) | | |__) | | |  | | | | __
+|  ___/  |  _  /  | |  | | | ||_ \ 
+| |      | | \ \  | |__| | | |__| | 
+|_|      |_|  \_\  \____/   \____/ 
+""")
+		choice = valid_input(menu_options, "Menu options: ")
+		match choice:
+			case "new":
+				if exists("game.dat"):
+					remove("game.dat")
+				clear()
+				print("You wake up and remember nothing besides a goal: to obtain the Prog.")
+				game_loop()
+			case "quit":
+				print("Thanks for playing!")
+			case "save":
 				save()
-				print("Thanks for playing")
+			case "load":
+				clear()
+				load()
+				game_loop()
+			case "resume":
+				game_loop()
+				menu_options.pop()
+
+if __name__ == "__main__":
+	main(player)
